6999ff3011fcf044ac51df3ec9f8673d70df41e9
willadam <willadamin@gmail.com>
2018-12-23 01:45:35 +0800

    first react dist push

---
+++ precache-manifest.9e35116593dc2c8d5566ea901fe8141f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc2af06674a02b73a240",
+    "url": "/static/js/main.bc2af066.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bc2af06674a02b73a240",
+    "url": "/static/css/main.45b272ce.chunk.css"
+  },
+  {
+    "revision": "2753462b933dc0bba9980deb31f7ae8e",
+    "url": "/index.html"
+  }
+];
